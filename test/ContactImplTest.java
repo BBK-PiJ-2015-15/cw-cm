5c13276c2508b8d9ce05d96e92ab876788a60a7c
Jose Massada <jose.massada@gmail.com>
2016-01-11 23:15:18 +0000

    cleaned up contact implementation test code

--- test/ContactImplTest.java
+++ test/ContactImplTest.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
@@ -5,57 +6,127 @@ import org.junit.*;
  * Implementation of the ContactImpl test class.
  */
 public class ContactImplTest {
-    @Test(expected=IllegalArgumentException.class)
-    public void constructingWithInvalidIdShouldThrow3() {
-        Contact contact = new ContactImpl(0, "name", "notes");
+    private static Random random;
+
+    private final String name = "John Doe";
+    private final String notes = "A note about John Doe";
+    private int id;
+    
+    /**
+     * Generates the random number seed.
+     */
+    @BeforeClass
+    public static void seed() {
+        random = new Random();
     }
     
-    @Test(expected=IllegalArgumentException.class)
-    public void constructingWithInvalidIdShouldThrow2() {
-        Contact contact = new ContactImpl(0, "name");
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void constructingWithNullNameShouldThrow3() {
-        Contact contact = new ContactImpl(1, null, "notes");
-    
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void constructingWithNullNameShouldThrow2() {
-        Contact contact = new ContactImpl(1, null);
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void constructingWithNullNotesShouldThrow3() {
-        Contact contact = new ContactImpl(1, "name", null);
+    /**
+     * Generates a random id in the range 1 to Integer.MAX_VALUE.
+     */
+    @Before
+    public void generateRandomId() {
+        id = random.nextInt(Integer.MAX_VALUE - 1) + 1;
     }
     
     @Test
-    public void retrievedIdNameAndOrNotesShouldBeTheSame() {
-        Contact contact = new ContactImpl(1, "name", "notes");
-        assertEquals(contact.getId(), 1);
-        assertEquals(contact.getName(), "name");
-        assertEquals(contact.getNotes(), "notes");
+    public void testNewContactWithNegativeIdShouldThrow() {
+        // test constructor with three arguments
+        assertThrowsException(IllegalArgumentException.class, () -> {
+            Contact contact = new ContactImpl(-1, name, notes);
+        });
+
+        // test constructor with two arguments
+        assertThrowsException(IllegalArgumentException.class, () -> {
+            Contact contact = new ContactImpl(-1, name);
+        });
+    }
+    
+    @Test
+    public void testNewContactWithZeroIdShouldThrow() {
+        // test constructor with three arguments
+        assertThrowsException(IllegalArgumentException.class, () -> {
+            Contact contact = new ContactImpl(0, name, notes);
+        });
+
+        // test constructor with two arguments
+        assertThrowsException(IllegalArgumentException.class, () -> {
+            Contact contact = new ContactImpl(0, name);
+        });
+    }
+    
+    @Test
+    public void testNewContactWithNullNameShouldThrow() {
+        // test constructor with three arguments
+        assertThrowsException(NullPointerException.class, () -> {
+            Contact contact = new ContactImpl(id, null, notes);
+        });
         
-        contact = new ContactImpl(1, "name");
-        assertEquals(contact.getId(), 1);
-        assertEquals(contact.getName(), "name");
+        // test constructor with two arguments
+        assertThrowsException(NullPointerException.class, () -> {
+            Contact contact = new ContactImpl(id, null);
+        });
+    }
+    
+    @Test(expected=NullPointerException.class)
+    public void testNewContactWithNullNotesShouldThrow() {
+        Contact contact = new ContactImpl(id, name, null);
+    }
+    
+    @Test
+    public void testNewContactWithNameAndNotes() {
+        // test constructor with three arguments
+        Contact contact = new ContactImpl(id, name, notes);
+        assertEquals(contact.getId(), id);
+        assertEquals(contact.getName(), name);
+        assertEquals(contact.getNotes(), notes);
+        
+        // test constructor with two arguments
+        contact = new ContactImpl(id, name);
+        assertEquals(contact.getId(), id);
+        assertEquals(contact.getName(), name);
         assertEquals(contact.getNotes(), "");
     }
     
     @Test
-    public void retrievingAddedNotesShouldBeTheSame() {
-        Contact contact = new ContactImpl(1, "name");
-        assertEquals(contact.getId(), 1);
-        assertEquals(contact.getName(), "name");
-        assertEquals(contact.getNotes(), "");
+    public void testNewContactWithAddedNotes() {
+        final String extraNotes[] = { "new note about contact", "end note" };
+    
+        // test constructor with three arguments
+        Contact contact = new ContactImpl(id, name, notes);
         
-        contact.addNotes("new note about contact");
-        assertEquals(contact.getNotes(), "new note about contact");
+        contact.addNotes(extraNotes[0]);
+        assertEquals(contact.getNotes(), extraNotes[0]);
         
-        contact.addNotes("append new note");
-        assertEquals(contact.getNotes(), "new note about contact\n" +
-            "append new note");
+        contact.addNotes(extraNotes[1]);
+        assertEquals(contact.getNotes(), notes + "\n" + extraNotes[0] + "\n" +
+            extraNotes[1]);
+        
+        // test constructor with two arguments
+        contact = new ContactImpl(id, name);
+        
+        contact.addNotes(extraNotes[0]);
+        assertEquals(contact.getNotes(), extraNotes[0]);
+        
+        contact.addNotes(extraNotes[1]);
+        assertEquals(contact.getNotes(), extraNotes[0] + "\n" + extraNotes[1]);
+    }
+    
+    /**
+     * Helper assertion method to assert that a runnable code throws a
+     * particular exception.
+     *
+     * This method allows a single JUnit test method to assert for multiple
+     * exceptions.
+     */
+    private void assertThrowsException(
+            Class<? extends Exception> expectedExceptionClass,
+            Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            if (expectedExceptionClass.isInstance(e))
+                return;
+        }
+        fail();
     }
 }
